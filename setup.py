32960ddb2e01fe2aa4039a50c3bd18ae66325a09
AlbertoMack <116552864+AlbertoPad@users.noreply.github.com>
2023-02-28 16:48:01 +0100

    asd

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="Albert_Library",
+    version="0.1",
+    description="A library for pln",
+    author="Alberto Pad",
+    author_email="apadillanieto@gmail.com",
+    url="https://github.com/AlbertoPad/AlbertLibrary",
+    packages=["AlbertLibrary"],
+    install_requires=["nltk", "spacy"],
+)
